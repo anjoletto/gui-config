2a2e6bd6031765ea4cf581ae1945144ba5a345a2
Leonardo <43471239+anjoletto@users.noreply.github.com>
2021-04-24 17:53:50 -0300

    script to link files in correct place

---
+++ setup-links.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+ln -s $(pwd)/sxhkd $HOME/.config/sxhkd
+ln -s $(pwd)/polybar $HOME/.config/polybar
+ln -s $(pwd)/alacritty/alacritty.yml $HOME/.config/alacritty.yml
+ln -s $(pwd)/dunst $HOME/.config/dunst
+ln -s $(pwd)/rofi $HOME/.config/rofi
+#ln -s $(pwd)/x11/xresources $HOME/.xresources
+#ln -s $(pwd)/x11/xinitrc $HOME/.xinitrc
